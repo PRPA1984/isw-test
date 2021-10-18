2c4ff1987bede369ab7547ac0344eed98f762d1a
Guido Debandi <gdebandi12@gmail.com>
2021-10-18 17:15:46 -0300

    Actualizadas clases

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,14 +1,21 @@
 public class Persona {
-    public static final String nombre="Fideos Marolio 200gr";
+    private String nombre;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
 
     public boolean validar_max_nombre(){
-        if(nombre.length()<=30){
+        if(this.nombre.length()<=30){
             return true;
         }
         return false;
     }
     public boolean validar_min_nombre(){
-        if(nombre.length()>8){
+        if(this.nombre.length()>8){
             return true;
         }
         return false;
--- src/test/java/Isw.java
+++ src/test/java/Isw.java
@@ -3,36 +3,35 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class Isw {
-    private boolean esperado;
-    private boolean resultado;
+
     @Test
     public void verificar_que_cantidad_sea_menor_stock() {
         Producto producto = new Producto("Fideos");
-        esperado = true;
-        resultado = producto.validar_stock(3);
+        boolean esperado = true;
+        boolean resultado = producto.validar_stock(3);
         assertEquals(esperado,resultado);
     }
 
     @Test
     public void verificar_que_no_pueda_validar_un_carrito_sir_productos() {
         Carrito carrito = new Carrito();
-        esperado = false;
-        resultado = carrito.validar_carrito_no_nulo();
+        boolean esperado = false;
+        boolean resultado = carrito.validar_carrito_no_nulo();
         assertEquals(esperado,resultado);
     }
 
     @Test
     public void verificar_que_el_nombre_contenga_menos_de_30_caracteres(){
-        Persona persona=new Persona();
-        esperado=true;
-        resultado=persona.validar_max_nombre();
+        Persona persona=new Persona("Fideos Marolio 200gr");
+        boolean esperado=true;
+        boolean resultado=persona.validar_max_nombre();
         assertEquals(esperado,resultado);
     }
     @Test
     public void verificar_que_el_nombre_contenga_mas_de_8_caracteres(){
-        Persona persona=new Persona();
-        esperado=true;
-        resultado=persona.validar_min_nombre();
+        Persona persona=new Persona("Fideos Marolio 200gr");
+        boolean esperado=true;
+        boolean resultado=persona.validar_min_nombre();
         assertEquals(esperado,resultado);
     }
 }
