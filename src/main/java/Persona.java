118ce22a33383d1f9cf970f887449d997a44bd2f
Tomas <mallartomas@gmail.com>
2021-10-19 17:46:01 -0300

    arreglo tomi

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -2,7 +2,7 @@ public class Persona {
     private String contraseña;
     public boolean validar_max_contraseña(String contraseña) {
 
-        if(contraseña.length() < 8){
+        if(contraseña.length() > 8){
             return true;
         }
         else{
